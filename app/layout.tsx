811242c3d5f95e09728afba182c4bdcfaf0c3918
김민경 <infinitelove367@gmail.com>
2026-02-12 15:01:34 +0900

    🍎add baseline commit for performance measurement

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { firacode_light } from "../styles/font";
 import { ThemeProvider } from "next-themes";
 import "../styles/global.css";
 
+// baseline for mobile performance measurement
+
 export const metadata = {
   metadataBase: new URL("https://minkyoung-kim.vercel.app"),
   title: "Kim Min Kyoung",
